a496e0bbcc96b395e9248c912cbddb955f4cac8f
Tiagoladrn <santiagobol48@gmail.com>
2024-07-04 16:27:46 -0500

    Agregado proyecto AngularPrueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Importa Validators desde @angular/forms
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'AppFormulario';
+
+  public formularioUsuario: FormGroup;
+  public enviado = false;
+  constructor(private fb: FormBuilder) {
+
+    this.formularioUsuario = this.fb.group({
+      nombre: ["", Validators.required],      // Agrega Validators.required
+      correo: ["", [Validators.required, Validators.email]],      // Agrega Validators.required
+      numero: ["", Validators.required],      // Agrega Validators.required
+      direccion: ["", Validators.required]    // Agrega Validators.required
+    });
+   }
+
+  ngOnInit(): void {
+
+  }
+
+  submitForm(){
+    if(this.formularioUsuario.valid){
+      this.enviado = true;
+    }else{
+      this.enviado = false;
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'; // Importa ReactiveFormsModule para los formularios reactivos
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule, // Agrega ReactiveFormsModule aquí
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
